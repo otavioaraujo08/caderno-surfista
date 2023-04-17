31cb8fb02080df5c1788c798491d5542463945f9
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-04-17 10:53:20 -0300

    temp: update stormGlass test

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,4 +1,10 @@
 import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+// Limpa o cache do módulo
+jest.mock('axios');
 
 describe('StormGlass client', () => {
     it('should return the normalized forecast from the StormGlass service', async () => {
@@ -6,11 +12,14 @@ describe('StormGlass client', () => {
         const lat = -33.792726;
         const lng = 151.289824;
 
+        // Quando o axios for chamado, retorne o objeto abaixo
+        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
         // Definindo o mock da resposta
-        const stormGlass = new StormGlass();
+        const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, lng);
 
         // Definindo o mock da resposta
-        expect(response).toEqual({});
+        expect(response).toEqual(stormGlassNormalized3HoursFixture);
     });
 });
