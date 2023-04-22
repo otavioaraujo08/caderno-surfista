9cfae49402859796409f1ad38dd54a804c5a33af
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-04-22 19:47:56 -0300

    feat: creating test to verify if filter is working

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -9,12 +9,11 @@ jest.mock('axios');
 describe('StormGlass client', () => {
     // Criando um mock do axios, com os tipos do axios, para forçar o typescript a inferir o tipo do meu dadod
     const mockedAxios = axios as jest.Mocked<typeof axios>;
+    // Definindo o mock da requisição
+    const lat = -33.792726;
+    const lng = 151.289824;
 
     it('should return the normalized forecast from the StormGlass service', async () => {
-        // Definindo o mock da requisição
-        const lat = -33.792726;
-        const lng = 151.289824;
-
         // Quando o axios for chamado, retorne o objeto abaixo
         mockedAxios.get.mockResolvedValue({
             data: stormGlassWeather3HoursFixture,
@@ -26,5 +25,30 @@ describe('StormGlass client', () => {
 
         // Definindo o mock da resposta
         expect(response).toEqual(stormGlassNormalized3HoursFixture);
+    });
+
+    // Dados que não possuem todas as chaves, devem ser excluidos
+    it('should exclude incomplete data points', async () => {
+        const incompleteResponse = {
+            hours: [
+                {
+                    winDirection: {
+                        noaa: 300,
+                    },
+
+                    time: '2020-04-26T00:00:00+00:00',
+                },
+            ],
+        };
+
+        // Testando se a resposta está sendo filtrada
+        mockedAxios.get.mockResolvedValue({ data: incompleteResponse });
+
+        // Definindo o mock da resposta
+        const stormGlass = new StormGlass(mockedAxios);
+        const response = await stormGlass.fetchPoints(lat, lng);
+
+        // Espero que a resposta seja um array vazio
+        expect(response).toEqual([]);
     });
 });
