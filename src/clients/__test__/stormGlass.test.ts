4a5aea074ba7b988bc811dfd57c40358adbbfa6f
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-04-21 22:10:34 -0300

    fix: updating mockedAxios to best practices

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -7,18 +7,21 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+    // Criando um mock do axios, com os tipos do axios, para forçar o typescript a inferir o tipo do meu dadod
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
+
     it('should return the normalized forecast from the StormGlass service', async () => {
         // Definindo o mock da requisição
         const lat = -33.792726;
         const lng = 151.289824;
 
         // Quando o axios for chamado, retorne o objeto abaixo
-        axios.get = jest
-            .fn()
-            .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+        mockedAxios.get.mockResolvedValue({
+            data: stormGlassWeather3HoursFixture,
+        });
 
         // Definindo o mock da resposta
-        const stormGlass = new StormGlass(axios);
+        const stormGlass = new StormGlass(mockedAxios);
         const response = await stormGlass.fetchPoints(lat, lng);
 
         // Definindo o mock da resposta
