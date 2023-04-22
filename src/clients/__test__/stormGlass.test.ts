9c75f8c93deac8fffb4cad856fa1ad300caae77e
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-04-22 20:20:10 -0300

    feat: creating test to check if the request limit was reached

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -60,4 +60,25 @@ describe('StormGlass client', () => {
             'Unexpected error when trying to communicate to StormGlass: Network Error'
         );
     });
+
+    it('should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
+        class FakeAxiosError extends Error {
+            constructor(public response: object) {
+                super();
+            }
+        }
+
+        mockedAxios.get.mockRejectedValue(
+            new FakeAxiosError({
+                status: 429,
+                data: { errors: ['Rate Limit reached'] },
+            })
+        );
+
+        const stormGlass = new StormGlass(mockedAxios);
+
+        await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+            'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
+        );
+    });
 });
