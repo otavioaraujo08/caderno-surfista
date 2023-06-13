d9a6a81e158cd3d1874733c027e55853b2b20efe
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-13 17:21:18 -0300

    fix: add error threatment and update catch error

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,22 +1,18 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { User } from '@src/models/user';
+import { BaseController } from '.';
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
     @Post('')
     public async create(req: Request, res: Response): Promise<void> {
         try {
             const user = new User(req.body);
             const newUser = await user.save();
-
             res.status(201).send(newUser);
-        } catch (error) {
-            if (error instanceof Error) {
-                res.status(400).send({
-                    error: error.message,
-                });
-            }
+        } catch (error: any) {
+            this.sendCreatedUpdateErrorResponse(res, error);
         }
     }
 }
