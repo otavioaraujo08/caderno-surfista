f47e8aee6f3f640f35f628067fd507c55c40583e
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-13 15:10:24 -0300

    feat: create user  and userModel interface

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,10 @@
+import { Document } from 'mongoose';
+
+export interface User {
+    _id?: string;
+    name: string;
+    email: string | boolean | undefined;
+    password: string;
+}
+
+export interface UserModel extends Omit<User, '_id'>, Document {}
