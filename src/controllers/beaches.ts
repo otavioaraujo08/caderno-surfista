98a834d95a7eff6f0d64a2e2f6434ac258535a06
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-13 13:21:18 -0300

    feat: error treatment on post function

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,25 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
+import { Beach } from '@src/models/beach';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
     @Post('')
     public async create(req: Request, res: Response): Promise<void> {
-        res.status(201).send({ ...req.body, id: 'fake-id' });
+        try {
+            // Definindo o corpo da requisição como um Beach
+            const beach = new Beach(req.body);
+            const result = await beach.save();
+
+            // O que iremos retornar ao usuário
+            res.status(201).send(result);
+        } catch (error) {
+            if (error instanceof mongoose.Error.ValidationError) {
+                res.status(422).send({ error: error.message });
+            } else {
+                res.status(500).send({ error: 'Internal Server Error' });
+            }
+        }
     }
 }
