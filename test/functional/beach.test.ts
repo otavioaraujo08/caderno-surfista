709b1dce8fd80679d226ee6091105fbda7826c12
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-13 13:35:19 -0300

    feat: create test when send invalid field type

--- test/functional/beach.test.ts
+++ test/functional/beach.test.ts
@@ -16,5 +16,24 @@ describe('Beaches functional tests', () => {
             expect(response.status).toBe(201);
             expect(response.body).toEqual(expect.objectContaining(newBeach));
         });
+
+        it('Should return 422 when there is a validation error', async () => {
+            const newBeach = {
+                lat: 'Invalid String',
+                lng: -48.812844829117886,
+                name: 'Beja',
+                position: 'N',
+            };
+
+            const response = await global.testRequest
+                .post('/beaches')
+                .send(newBeach);
+
+            // Esperamos que a resposta seja 422 e que seja
+            expect(response.status).toBe(422);
+            expect(response.body).toEqual({
+                error: 'Beach validation failed: lat: Cast to Number failed for value "Invalid String" (type string) at path "lat"',
+            });
+        });
     });
 });
