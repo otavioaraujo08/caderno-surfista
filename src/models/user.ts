15c0cea7bce89311c26e471f19907f7d907567f8
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-13 15:11:00 -0300

    feat: create user models

---
+++ src/models/user.ts
@@ -0,0 +1,25 @@
+import { UserModel } from '@src/interfaces/IUser';
+import mongoose, { Model } from 'mongoose';
+
+const schema = new mongoose.Schema<UserModel>(
+    {
+        name: { type: String, required: true },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: { type: String, required: true },
+    },
+    {
+        toJSON: {
+            transform: (_, ret): void => {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.__v;
+            },
+        },
+    }
+);
+
+export const User: Model<UserModel> = mongoose.model('User', schema);
