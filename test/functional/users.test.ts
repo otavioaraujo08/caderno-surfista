4088c71caf53fb74acda787a39f61198e256d221
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-13 16:06:05 -0300

    feat: create test when user is invalid

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -22,5 +22,22 @@ describe('Users functional test', () => {
             expect(response.status).toBe(201);
             expect(response.body).toEqual(expect.objectContaining(newUser));
         });
+
+        it('should throw a error when there is a validation error', async () => {
+            const newUser = {
+                email: 'otavioaraujo@mail.com',
+                password: '1234',
+            };
+
+            const response = await global.testRequest
+                .post('/users')
+                .send(newUser);
+
+            // Esperamos que o status retornado seja 201 (Created), e que o body seja um objeto contendo os campos de newUser
+            expect(response.status).toBe(400);
+            expect(response.body).toEqual({
+                error: 'User validation failed: name: Path `name` is required.',
+            });
+        });
     });
 });
