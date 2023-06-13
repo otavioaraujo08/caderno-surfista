2b5c97e0b97676c7d77f6cdbe244b956a5ba0468
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-13 13:35:53 -0300

    feat: add function to clear database when init test

--- test/functional/beach.test.ts
+++ test/functional/beach.test.ts
@@ -1,4 +1,9 @@
+import { Beach } from '@src/models/beach';
+
 describe('Beaches functional tests', () => {
+    // Antes de cada teste, limpar o banco de dados
+    beforeAll(async () => await Beach.deleteMany({}));
+
     describe('When creating a beach', () => {
         it('should create a beach with success', async () => {
             const newBeach = {
