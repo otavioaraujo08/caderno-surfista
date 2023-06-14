8a4a75de8ec99b8b1ff3e9d437539869dfbe0174
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-14 19:01:13 -0300

    feat: create generateToken function

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export default class AuthService {
     // Criando uma função para incriptar a senha do usuário
@@ -15,5 +17,15 @@ export default class AuthService {
         hashedPassword: string
     ): Promise<boolean> {
         return await bcrypt.compare(password, hashedPassword);
+    }
+
+    // Criando uma função para gerar o token do usuário
+    public static generateToken(payload: object): string {
+        const key: string = config.get('App.auth.key');
+        const expiresIn: number = config.get('App.auth.tokenExpiresIn');
+
+        return jwt.sign(payload, key, {
+            expiresIn: expiresIn,
+        });
     }
 }
