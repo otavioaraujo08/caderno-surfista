39e64b2f301ca40f95ca8a917e748cb782cfd5c1
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-14 19:00:47 -0300

    feat: create authenticate method

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,7 @@ import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from '.';
+import AuthService from '@src/services/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -14,5 +15,28 @@ export class UsersController extends BaseController {
         } catch (error: any) {
             this.sendCreatedUpdateErrorResponse(res, error);
         }
+    }
+
+    @Post('authenticate')
+    public async authenticate(req: Request, res: Response): Promise<Response> {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).send({
+                code: 401,
+                error: 'User not found!',
+            });
+        }
+
+        if (!(await AuthService.comparePassword(password, user.password))) {
+            return res.status(401).send({
+                code: 401,
+                error: 'Password does not match!',
+            });
+        }
+
+        const token = AuthService.generateToken(user.toJSON());
+        return res.status(200).send({ token: token });
     }
 }
