3dd3249c45015c5db04ec243e8b59cd80f0796e0
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-19 16:28:54 -0300

    fix: filter beach by user

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
+import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
 import { Request, Response } from 'express';
@@ -6,18 +7,18 @@ import { Request, Response } from 'express';
 const forecast = new Forecast();
 
 @Controller('forecast')
+@ClassMiddleware(authMiddleware)
 export class ForecastController {
     @Get('')
     public async getForecastLoggedUser(
-        _: Request,
+        req: Request,
         response: Response
     ): Promise<void> {
         try {
-            const beaches = await Beach.find({});
+            const beaches = await Beach.find({ user: req.decoded?.id });
             const forecastData = await forecast.processForecastForBeaches(
                 beaches
             );
-
             response.status(200).send(forecastData);
         } catch (error) {
             response.status(500).send({ error: 'Something went wrong' });
