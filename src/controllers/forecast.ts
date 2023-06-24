d46d39eaf8324dbd0799525bd46fbb062c682d51
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-24 17:32:46 -0300

    fix: update error message

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -4,26 +4,30 @@ import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
 import { Request, Response } from 'express';
 import logger from '@src/logger';
+import { BaseController } from '.';
 
 const forecast = new Forecast();
 
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
-export class ForecastController {
+export class ForecastController extends BaseController {
     @Get('')
     public async getForecastLoggedUser(
-        req: Request,
+        request: Request,
         response: Response
     ): Promise<void> {
         try {
-            const beaches = await Beach.find({ user: req.decoded?.id });
+            const beaches = await Beach.find({ user: request.decoded?.id });
             const forecastData = await forecast.processForecastForBeaches(
                 beaches
             );
             response.status(200).send(forecastData);
         } catch (error) {
             logger.error(error);
-            response.status(500).send({ error: 'Something went wrong' });
+            this.sendCreatedUpdateErrorResponse(response, {
+                code: 500,
+                error: 'Something went wrong!',
+            });
         }
     }
 }
