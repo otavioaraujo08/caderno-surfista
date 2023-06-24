cebc5a6622e56d9ce29627f05e0baa200b5a4b62
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-24 17:32:36 -0300

    fix: update error message

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from '.';
 import AuthService from '@src/services/auth';
+import ApiError from '@src/utils/errors/api-error';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -23,17 +24,21 @@ export class UsersController extends BaseController {
         const user = await User.findOne({ email });
 
         if (!user) {
-            return res.status(401).send({
-                code: 401,
-                error: 'User not found!',
-            });
+            return res.status(401).send(
+                ApiError.format({
+                    code: 401,
+                    message: 'User not found!',
+                })
+            );
         }
 
         if (!(await AuthService.comparePassword(password, user.password))) {
-            return res.status(401).send({
-                code: 401,
-                error: 'Password does not match!',
-            });
+            return res.status(401).send(
+                ApiError.format({
+                    code: 401,
+                    message: 'Password does not match!',
+                })
+            );
         }
 
         const token = AuthService.generateToken(user.toJSON());
