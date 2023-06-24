0209ad7c40d171769fda527830b84c8ee2cd9613
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-24 17:30:55 -0300

    fix: update test to new error format

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -47,7 +47,9 @@ describe('Users functional test', () => {
             expect(response.status).toBe(422);
             expect(response.body).toEqual({
                 code: 422,
-                error: 'User validation failed: name: Path `name` is required.',
+                error: 'Unprocessable Entity',
+                message:
+                    'User validation failed: name: Path `name` is required.',
             });
         });
 
@@ -66,7 +68,9 @@ describe('Users functional test', () => {
             expect(response.status).toBe(409);
             expect(response.body).toEqual({
                 code: 409,
-                error: 'User validation failed: email: already exists in the database.',
+                error: 'Conflict',
+                message:
+                    'User validation failed: email: already exists in the database.',
             });
         });
 
