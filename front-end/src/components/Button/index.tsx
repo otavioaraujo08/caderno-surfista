913249029a68355506e289bff6706dd30e719912
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-29 17:09:42 -0300

    feat: add new props on button component

--- front-end/src/components/Button/index.tsx
+++ front-end/src/components/Button/index.tsx
@@ -2,18 +2,20 @@ import './styles.css';
 import Button from '@mui/material/Button';
 interface ButtonProps {
     label: string;
-    onClick: () => void;
+    onClick?: () => void;
     style?: React.CSSProperties;
+    type?: 'button' | 'submit' | 'reset' | undefined;
 }
 
 export const ButtonComponent = (Props: ButtonProps) => {
-    const { label, onClick, style } = Props;
+    const { label, onClick, style, type } = Props;
 
     return (
         <Button
             variant="contained"
             onClick={onClick}
             style={style}
+            type={type}
             className="button"
         >
             {label}
