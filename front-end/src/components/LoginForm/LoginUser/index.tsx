6f49bc8b6fda768a3c24d465d1817f37fc15b2a3
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-29 17:11:10 -0300

    temp: create login function

--- front-end/src/components/LoginForm/LoginUser/index.tsx
+++ front-end/src/components/LoginForm/LoginUser/index.tsx
@@ -1,35 +1,87 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { ButtonComponent } from '@/components/Button';
 import './styles.css';
-import { Typography, Box, TextField } from '@mui/material';
+import { Typography, Box, TextField, Alert } from '@mui/material';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { loginSchema } from './schema';
+import { userService } from '@/services/user';
+import { AlertMessage } from '@/components/AlertMessage';
 
-export const LoginUser: React.FC = () => {
-    const handleLogin = () => {
-        alert('Login');
+interface LoginFunctionProps {
+    email: string;
+    password: string;
+}
+
+interface LoginUserProps {
+    setUserClicked: (value: boolean) => void;
+}
+
+export const LoginUser = (props: LoginUserProps) => {
+    const { setUserClicked } = props;
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(loginSchema),
+    });
+
+    const handleLogin: SubmitHandler<LoginFunctionProps> = async (data) => {
+        try {
+            await userService.LoginUser(data);
+        } catch (error: any) {
+            return <Alert>text=Usuário não encontrado</Alert>;
+        }
     };
 
     return (
-        <Box className="boxForm">
+        <Box
+            className="boxForm"
+            component="form"
+            onSubmit={handleSubmit(handleLogin)}
+        >
             <Typography align="center" className="formTitle">
                 Realizar login
             </Typography>
 
             <Box className="boxInput">
                 <label className="label">Email</label>
-                <TextField label="Digite seu email" variant="outlined" />
+                <TextField
+                    type="string"
+                    label="Digite seu email"
+                    variant="outlined"
+                    {...register('email')}
+                    error={!!errors?.email}
+                    helperText={errors?.email?.message}
+                />
             </Box>
 
             <Box className="boxInput">
                 <label className="label">Senha</label>
-                <TextField label="Digite sua senha" variant="outlined" />
+                <TextField
+                    type="string"
+                    label="Digite sua senha"
+                    variant="outlined"
+                    {...register('password')}
+                    error={!!errors?.password}
+                    helperText={errors?.password?.message}
+                />
             </Box>
 
             <ButtonComponent
                 label="Entrar"
-                onClick={handleLogin}
+                type="submit"
                 style={{
                     marginTop: '20px',
                 }}
             />
+
+            <Typography
+                onClick={() => setUserClicked(false)}
+                className="return"
+            >
+                Retornar
+            </Typography>
         </Box>
     );
 };
