695b55450fd531fbc1a0f6c5c07cb2de29f34ff1
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-06-30 17:19:24 -0300

    feat: create authenticate service

---
+++ front-end/src/services/authenticate.ts
@@ -0,0 +1,12 @@
+import { UserLogin } from '@/interfaces/IUser';
+
+import { api as apiService, ApiService, defaultUrl } from './api';
+
+class AuthenticationService {
+    constructor(private readonly api: ApiService) {}
+
+    public login = async (login: UserLogin): Promise<void> => {
+        return this.api.post(`${defaultUrl}/auth/login`, login);
+    };
+}
+export const authenticationService = new AuthenticationService(apiService);
