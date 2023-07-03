5271b402b183d7e20eb39b588d9cf73a322ce68a
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-03 17:20:27 -0300

    feat: update usePopup function

--- front-end/src/hooks/UsePopup.tsx
+++ front-end/src/hooks/UsePopup.tsx
@@ -1,9 +1,10 @@
-import { Alert } from '@mui/material';
-import React, { useState } from 'react';
+import { Popup } from '@/components/Popup';
+import React, { useEffect, useState } from 'react';
 
-type AlertType = 'error' | 'warning' | 'info' | 'success';
+type AlertType = 'error' | 'success';
 
 interface Popup {
+    title: string;
     message: string;
     type: AlertType;
 }
@@ -11,23 +12,38 @@ interface Popup {
 export const UsePopup = () => {
     const [popup, setPopup] = useState<Popup | null>(null);
 
-    const showAlert = (message: string, type: AlertType = 'info') => {
-        setPopup({ message, type });
+    const showAlert = (
+        title = 'Erro ao criar usuário',
+        message: string,
+        type: AlertType = 'success'
+    ) => {
+        setPopup({ title, message, type });
     };
 
     const hideAlert = () => {
         setPopup(null);
     };
 
+    useEffect(() => {
+        if (!popup) return;
+
+        setTimeout(() => {
+            hideAlert();
+        }, 6000);
+    }, [popup]);
+
     const PopupComponent = () => {
         if (!popup) return null;
 
         return (
-            <Alert severity={popup.type} onClose={hideAlert}>
-                {popup.message}
-            </Alert>
+            <Popup
+                title={popup.title}
+                message={popup.message}
+                type={popup.type}
+                onClose={hideAlert}
+            />
         );
     };
 
-    return { showAlert, hideAlert, PopupComponent };
+    return { showAlert, PopupComponent };
 };
