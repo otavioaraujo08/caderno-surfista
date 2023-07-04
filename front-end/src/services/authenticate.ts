b18ca1f3061239f9c9771ce7f18ef189d7f94ffd
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-04 17:24:59 -0300

    fix: incorrect url

--- front-end/src/services/authenticate.ts
+++ front-end/src/services/authenticate.ts
@@ -6,7 +6,7 @@ class AuthenticationService {
     constructor(private readonly api: ApiService) {}
 
     public login = async (login: UserLogin): Promise<void> => {
-        return this.api.post(`${defaultUrl}/auth/login`, login);
+        return this.api.post(`${defaultUrl}/users/authenticate`, login);
     };
 }
 export const authenticationService = new AuthenticationService(apiService);
