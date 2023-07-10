d856bb57f8d226e0c70af9131361fd74ac1238b4
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-09 23:35:48 -0300

    feat: add return when login is called

--- front-end/src/services/authenticate.ts
+++ front-end/src/services/authenticate.ts
@@ -1,11 +1,12 @@
 import { UserLogin } from '@/interfaces/IUser';
 
 import { api as apiService, ApiService, defaultUrl } from './api';
+import { LoginResponse } from '@/interfaces/ILogin';
 
 class AuthenticationService {
     constructor(private readonly api: ApiService) {}
 
-    public login = async (login: UserLogin): Promise<void> => {
+    public login = async (login: UserLogin): Promise<LoginResponse> => {
         return this.api.post(`${defaultUrl}/users/authenticate`, login);
     };
 }
