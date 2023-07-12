354065ddf7ddeb2192107fd1d191f767f060429d
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-11 23:31:25 -0300

    feat: add sidebar header to principal component

--- front-end/src/components/Sidebar/index.tsx
+++ front-end/src/components/Sidebar/index.tsx
@@ -1,6 +1,10 @@
+import { Header } from './Header';
+
 export const Sidebar = () => {
     return (
         <>
+            <Header name="Ot[avio" title="Time Traveller" />
+
             <h1>Sidebar</h1>
             <p>Yes, this is a sidebar</p>
         </>
