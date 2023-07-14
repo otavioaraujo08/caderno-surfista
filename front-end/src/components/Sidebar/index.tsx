4ce1081a5935f85cd7719f4ec5c947674b618fc5
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-13 23:34:15 -0300

    temp: show user infos on sidebar

--- front-end/src/components/Sidebar/index.tsx
+++ front-end/src/components/Sidebar/index.tsx
@@ -1,11 +1,17 @@
+'use client';
 import { Box } from '@mui/material';
 import { Body } from './Body';
 import { Header } from './Header';
+import { useAppSelector } from '@/redux/store';
 
 export const Sidebar = () => {
+    const { username, title } = useAppSelector(
+        (state) => state.authReducer.value
+    );
+
     return (
         <Box>
-            <Header name="Ot[avio" title="Time Traveller" />
+            <Header name={username} title={title} />
 
             <Body />
         </Box>
