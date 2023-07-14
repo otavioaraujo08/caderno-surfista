107d20adb5cf9429f5f8587ed60c45e1c5354f32
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-14 20:39:34 -0300

    feat: create get user function

--- front-end/src/services/user.ts
+++ front-end/src/services/user.ts
@@ -5,6 +5,10 @@ import { api as apiService, ApiService, defaultUrl } from './api';
 class UserService {
     constructor(private readonly api: ApiService) {}
 
+    public getUsers = async (): Promise<void> => {
+        return this.api.get(`${defaultUrl}/users`);
+    };
+
     public register = async (register: UserRegister): Promise<void> => {
         return this.api.post(`${defaultUrl}/users`, register);
     };
