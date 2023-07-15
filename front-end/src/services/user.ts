e308e3dce6cc6d39253c5dbacdd05b3d64b49882
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-14 23:04:54 -0300

    feat: create getUserInfos function

--- front-end/src/services/user.ts
+++ front-end/src/services/user.ts
@@ -2,11 +2,26 @@ import { UserRegister } from '@/interfaces/IUser';
 
 import { api as apiService, ApiService, defaultUrl } from './api';
 
+interface getUserInfosResponse {
+    name: string;
+    email: string;
+}
+
 class UserService {
     constructor(private readonly api: ApiService) {}
 
     public getUsers = async (): Promise<void> => {
         return this.api.get(`${defaultUrl}/users`);
+    };
+
+    public getUserInfos = async (
+        email: string
+    ): Promise<getUserInfosResponse> => {
+        const config = {
+            params: { email },
+        };
+
+        return this.api.get(`${defaultUrl}/users`, config);
     };
 
     public register = async (register: UserRegister): Promise<void> => {
